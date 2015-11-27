8a0059a4e82be9bd930cfb84e367c4b2c518eb62
Sam Pratt <samj.pratt@gmail.com>
2015-11-27 16:27:58 +0000

    get user endpoint

--- Controller/User/Index.php
+++ Controller/User/Index.php
@@ -2,25 +2,97 @@
 
 namespace Expressly\Expressly\Controller\User;
 
+use Expressly\Entity\Address;
+use Expressly\Entity\Customer;
+use Expressly\Entity\Email;
+use Expressly\Entity\Phone;
+use Expressly\Expressly\Model\ResourceModel\Merchant\Collection as MerchantCollection;
+use Expressly\Presenter\CustomerMigratePresenter;
 use Expressly\Presenter\PingPresenter;
+use Magento\Customer\Model\CustomerRegistry;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
+    private $merchant;
+    private $customerRegistry;
 
-    public function __construct(Context $context)
-    {
+    public function __construct(
+        Context $context,
+        MerchantCollection $merchantCollection,
+        CustomerRegistry $customerRegistry
+    ) {
         parent::__construct($context);
+
+        // 1 = store_id
+        $this->merchant = $merchantCollection->getItemById(1)->getMerchant();
+        $this->customerRegistry = $customerRegistry;
     }
 
     public function execute()
     {
-        $presenter = new PingPresenter();
-
+        $emailAddress = $this->_request->getParam('email');
         $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
-        $result->setData($presenter->toArray());
+
+        try {
+            $magentoCustomer = $this->customerRegistry->retrieveByEmail($emailAddress);
+
+            if (!$magentoCustomer->isEmpty()) {
+                $customer = new Customer();
+                $customer
+                    ->setFirstName($magentoCustomer->getFirstname())
+                    ->setLastName($magentoCustomer->getLastname());
+
+                $email = new Email();
+                $email
+                    ->setAlias('default')
+                    ->setEmail($emailAddress);
+                $customer->addEmail($email);
+
+                $defaultBilling = $magentoCustomer->getDefaultBillingAddress();
+                $defaultShipping = $magentoCustomer->getDefaultShippingAddress();
+
+                foreach ($magentoCustomer->getAddresses() as $magentoAddress) {
+                    $address = new Address();
+                    $address
+                        ->setFirstName($magentoAddress->getFirstname())
+                        ->setLastName($magentoAddress->getLastname())
+                        ->setCompanyName($magentoAddress->getCompany())
+                        ->setAddress1($magentoAddress->getStreetLine(1))
+                        ->setAddress2($magentoAddress->getStreetLine(2))
+                        ->setCity($magentoAddress->getCity())
+                        ->setStateProvince($magentoAddress->getRegion())
+                        ->setCountry($magentoAddress->getCountry());
+
+                    $phone = new Phone();
+
+                    $customer->addPhone($phone);
+                    $address->setPhonePosition($customer->getPhoneIndex($phone));
+
+                    $primary = false;
+                    $type = null;
+                    if ($defaultBilling->getId() == $magentoAddress->getId()) {
+                        $primary = true;
+                        $type = Address::ADDRESS_BILLING;
+                    }
+                    if ($defaultShipping->getId() == $magentoAddress->getId()) {
+                        $primary = true;
+                        $type = ($type == Address::ADDRESS_BILLING) ? Address::ADDRESS_BOTH : Address::ADDRESS_SHIPPING;
+                    }
+
+                    $customer->addAddress($address, $primary, $type);
+                }
+
+                $presenter = new CustomerMigratePresenter($this->merchant, $customer, $emailAddress, 55);
+
+                $result->setData($presenter->toArray());
+            }
+        } catch (\Exception $e) {
+            // log error
+            $result->setData([]);
+        }
 
         return $result;
     }
