cd1ec3fea851713cfe078ef01ab92d15e86194b8
Dmitriy Britan <dmitriy.britan@gmail.com>
2015-12-04 19:33:09 +0200

    implemented showing the popup atop the default page instead of by itself (like in Magento extension)

--- Controller/Migration/Popup.php
+++ Controller/Migration/Popup.php
@@ -22,36 +22,49 @@ class Popup extends Action
     private $merchant;
     private $application;
 
+    /**
+     * Popup constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     * @param StoreManagerInterface $storeManager
+     * @param ForwardFactory $resultForwardFactory
+     * @param Application $application
+     */
     public function __construct(
         Context $context,
         PageFactory $pageFactory,
         StoreManagerInterface $storeManager,
         ForwardFactory $resultForwardFactory,
-        Collection $merchantCollection,
         Application $application
     ) {
         parent::__construct($context);
-        $this->pageFactory = $pageFactory;
+        $this->pageFactory          = $pageFactory;
         $this->resultForwardFactory = $resultForwardFactory;
-        $this->merchant = $merchantCollection->getItemById($storeManager->getWebsite()->getId())->getMerchant();
-        $this->application = $application;
+        $this->application          = $application;
     }
 
+    /**
+     * @return \Magento\Framework\Controller\Result\Forward|void
+     */
     public function execute()
     {
+
+        $app = $this->application->getApp();
+
+        $merchant = $app['merchant.provider']->getMerchant();
+
         $uuid = $this->_request->getParam('uuid');
-        $event = new CustomerMigrateEvent($this->merchant, $uuid);
-
+        $event = new CustomerMigrateEvent($merchant, $uuid);
+        $content = '';
         try {
-            $this->application['dispatcher']->dispatch(CustomerMigrationSubscriber::CUSTOMER_MIGRATE_POPUP, $event);
-
-            if ($event->isSuccessful()) {
+            $app['dispatcher']->dispatch(CustomerMigrationSubscriber::CUSTOMER_MIGRATE_POPUP, $event);
+            if (!$event->isSuccessful()) {
                 throw new GenericException(Router::processError($event));
             }
-
-            var_dump($event->getContent());
+            $content = $event->getContent();
         } catch (\Exception $e) {
             // log error
+            var_dump($e->getMessage());
         }
 
         $pageId = $this->_objectManager
@@ -60,6 +73,7 @@ class Popup extends Action
                 \Magento\Cms\Helper\Page::XML_PATH_HOME_PAGE,
                 \Magento\Store\Model\ScopeInterface::SCOPE_STORE
             );
+
         $page = $this->_objectManager->get(Page::class)->prepareResultPage($this, $pageId);
 
         if (!$page) {
@@ -69,6 +83,39 @@ class Popup extends Action
             return $forward;
         }
 
-        return $page;
+        // XML injection doesn't work to add this javascript as we're overriding the page completely
+        $js = '<script type="text/javascript">
+                    (function() {
+                        popupContinue = function (event) {
+                            event.style.display = \'none\';
+                            var loader = event.nextElementSibling;
+                            loader.style.display = \'block\';
+                            loader.nextElementSibling.style.display = \'none\';
+                            window.location.replace(window.location.origin + window.location.pathname + \'/migrate\');
+                        };
+                        popupClose = function (event) {
+                            window.location.replace(window.location.origin);
+                        };
+                        openTerms = function (event) {
+                            window.open(event.href, \'_blank\');
+                        };
+                        openPrivacy = function (event) {
+                            window.open(event.href, \'_blank\');
+                        };
+                        (function () {
+                            // make sure our popup is on top or hierarchy
+                            content = document.getElementById(\'xly\');
+                            document.body.insertBefore(content, document.body.children[0]);
+                        })();
+                    })();
+                </script>';
+
+
+        $page->renderResult($this->getResponse());
+
+        $this->getResponse()->appendBody($js);
+        $this->getResponse()->appendBody($content);
+
+        return;
     }
 }
